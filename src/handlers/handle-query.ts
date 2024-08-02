a38990ba5a2272280f749d6d678c9d4baeac39c2
aiman-al-masoud <aiman.almasoud01@universitadipavia.it>
2024-08-02 14:35:14 +0200

    update handle query...

--- src/handlers/handle-query.ts
+++ src/handlers/handle-query.ts
@@ -1,22 +1,34 @@
 import http from "node:http";
 import { URL } from "node:url";
 import * as queries from '../../dbschema/queries'
+import * as edgedb from "edgedb";
 
 
 export const handleQuery = async (req: http.IncomingMessage, res: http.ServerResponse) => {
 
     const url = new URL(req.url!, 'http://localhost:3000')
 
-    const searchParams = url.searchParams
+    const searchParams = cleanSearchParams(url.searchParams)
     const cookies = parseCookies(req.headers.cookie ?? '')
     const pathname = url.pathname
     const funcname = camelize(pathname.slice(1))
-
+    const authToken = cookies['edgedb-auth-token']
+    
     console.log('searchParams=', searchParams)
     console.log('cookies=', cookies)
     console.log('pathname=', pathname)
     console.log('funcname=', funcname)
-    console.log(queries[funcname])
+    console.log()
+
+    const client = edgedb.createClient().withGlobals({
+        "ext::auth::client_token": authToken,
+    });
+
+    // @ts-ignore
+    const func = queries[funcname]
+    const result = await func(client, searchParams)
+
+    console.log(result)
 
     res.statusCode = 200
     res.end()
@@ -31,3 +43,13 @@ function parseCookies(cookies:string){
 function camelize(name:string){
     return name.replace(/-./g, x=>x[1].toUpperCase())
 }
+
+function cleanSearchParams(sp:URLSearchParams){
+
+    const object = Object.fromEntries(sp)
+    const entries = Object.entries(object)
+    const cleanEntries = entries.map(([k,v])=>[k, isNaN(parseFloat(v)) ? v : parseFloat(v) ])
+    return Object.fromEntries(cleanEntries)
+}
+
+
--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,18 @@ import * as edgedb from "edgedb";
 import { createComment, createDislike, createPost, getPosts } from "../dbschema/queries";
 
 
-const auth_token = 'eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3MjM1NTI1NTEuNDkxMDAzLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjEwNzAxL2RiL21haW4vZXh0L2F1dGgiLCJzdWIiOiI0ODdhYzFmMC00ZTVmLTExZWYtYjcxZS1kMzRhOTlhNzUxMDgifQ.E2xUYxn1Wl_UxCd_0uMSp7S8h7DVb26j2m9M2gjdYiQ'
+const auth_token = 'eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3MjM4MDQ0ODUuNTgwMjUsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6MTA3MDEvZGIvbWFpbi9leHQvYXV0aCIsInN1YiI6IjQ4N2FjMWYwLTRlNWYtMTFlZi1iNzFlLWQzNGE5OWE3NTEwOCJ9.Ec3V6Iooqco-XiLj-7S-9vekEwVs5_7zgggeg10pWbs'
 
 const client = edgedb.createClient().withGlobals({
     "ext::auth::client_token": auth_token,
 });
 
-// const x = await createPost(client, {text: 'ciao mondo!'})
+const x = await createPost(client, {text: 'ciao mondo!'})
+console.log(x)
+
 
 // const x = await createComment(client, {text:'fai schifo', postId: '73fa157e-4f13-11ef-8e21-4ba0190019e5'})
 // console.log(x)
-
-
 
 // const x = await createDislike(client, {
 //     postId: '73fa157e-4f13-11ef-8e21-4ba0190019e5'
@@ -21,5 +21,5 @@ const client = edgedb.createClient().withGlobals({
 // console.log(x)
 
 
-const x = await getPosts(client, {limit: 100, offset: 0})
-console.log(x.at(-1))
+// const x = await getPosts(client, {limit: 100, offset: 0})
+// console.log(x.at(-1))
